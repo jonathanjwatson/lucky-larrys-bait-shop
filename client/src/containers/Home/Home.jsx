22cbe4ad10de55987926be62ab3b9e2343b11792
MBP16 <jonathanwatson@MBP16s-MacBook-Pro.local>
2021-02-03 18:34:12 -0500

    homepage image complete

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Logo from "../../assets/images/LuckyLarrysBaitShop.jpg";
 
 const Home = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col">
-                    <h1 className="center-align">Lucky Larry's Bait Shop</h1>
+                <div className="col s12 center-align">
+                    <img src={Logo} alt="Logo" style={{width: "100%"}}/>
                 </div>
             </div>
         </div>
